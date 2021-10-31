1bdd3b430262f0e2753b00054d8b2c885d846032
Guille Paz <pazguille@Guilles-MacBook-Pro.local>
2021-10-31 18:51:25 -0300

    Initial commit.

--- src/toggle-script.js
+++ src/toggle-script.js
@@ -1,4 +1,6 @@
 chrome.storage.local.get(['xbox-converter'], (flags) => {
-  chrome.storage.local.set({ 'xbox-converter': !flags['xbox-converter'] });
-  chrome.runtime.sendMessage({ message: 'udpateIcon', data: !flags['xbox-converter'] ? 'on' : '' });
+  chrome.storage.local.set({ 'xbox-converter': !flags['xbox-converter'] }, () => {
+    chrome.runtime.sendMessage({ message: 'udpateIcon', data: !flags['xbox-converter'] ? 'on' : '' });
+    window.location.reload();
+  });
 });
